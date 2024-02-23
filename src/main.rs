28096ffc74585a11f5b821bf60347141c96654a4
doraemon <doraemonkey@qq.com>
2024-02-23 14:17:01 +0800

    feat: test commit1

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    println!("{}", say_hello("github actions"));
+}
+
+pub fn say_hello(name: &str) -> String {
+    format!("Hello, {}!", name)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_say_hello() {
+        assert_eq!(say_hello("world"), "Hello, world!");
+    }
+}
